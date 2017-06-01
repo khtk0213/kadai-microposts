6f6680219c37986726c8b9f7362c18fdd16ef515
khtkmk <khtkmk0213@gmail.com>
2017-06-01 07:38:26 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  include SessionsHelper
+  
   private
 
   def require_user_logged_in
@@ -18,5 +21,7 @@ class ApplicationController < ActionController::Base
 
   def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :require_user_logged_in
-  
+  before_action :correct_user, only: [:destroy]
+
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -11,7 +12,7 @@ class MicropostsController < ApplicationController
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
-   end
+  end
 
   def destroy
     @micropost.destroy
@@ -24,7 +25,7 @@ class MicropostsController < ApplicationController
   def micropost_params
     params.require(:micropost).permit(:content)
   end
-  
+
   def correct_user
     @micropost = current_user.microposts.find_by(id: params[:id])
     unless @micropost
